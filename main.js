12fc8faee4c0ad41ee976f8f16755895757dde5e
Carl Cody <CarlCody34@gmail.com>
2019-04-19 13:15:55 -0400

    third commit getting there

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let i = 0;
    function buttonClick() {
-       document.getElementById('inc',"corp").value = ++i;
+       document.getElementById('inc','corp').value = ++i;
+   }
+  //ANOTHER METHOD 
+//selct image
+   // const cat = document.QuerySelector("img")
+//counter for counting clicks
+   //let counter = 0;
+   //const cat = document.QuerySelector("click")
+
+   /* cat.addEventListener("click", ()=>{
+       counter++;
+       click.
    }
