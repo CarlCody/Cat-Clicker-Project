ea55f125c8ee9b9aadcfdfe8fa5c636acd027154
Carl Cody <CarlCody34@gmail.com>
2019-04-22 11:05:27 -0400

    changing into ES6

--- main.js
+++ main.js
@@ -22,3 +22,32 @@ let i = 0;
        click.
    }
 */
+
+// ES6 Method 
+/* const comic1 ={
+    name: "anna",
+    picture:"marvel.jpeg",
+    clicks: 0
+} */
+ /* const comic1container=document.querySelector                 */
+ // const comic1 =  document.createElement
+ //comic1.textContent=comic1.name
+  
+ /* const comic1 = {
+     name:"Black Spiderman",
+     picture:"cat.comic.jpg",
+     clicks:0
+ }
+  const comic2 = {
+     name:"Avengers",
+     picture:"cat.comic2.jpg",
+     clicks:0
+ }
+ */
+  //make the cast as a list
+// const cats =[cat1,cat2]
+  //Using loop to add eventListener on everycat in the list
+/* for (cat of cats){
+    cat.addEventListener()
+}
+*/
